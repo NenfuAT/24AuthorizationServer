59b7b7c04cbbbb78818bd7606bf0e15fe6a5dc31
k21091 <k21091kk@aitech.ac.jp>
2024-04-26 18:13:47 +0900

    add:構造体の追加

---
+++ src/model/user.go
@@ -0,0 +1,12 @@
+package model
+
+type User struct {
+	Id         int
+	Name       string
+	Password   string
+	Sub        string
+	NameJa     string
+	GivenName  string
+	FamilyName string
+	Locale     string
+}
